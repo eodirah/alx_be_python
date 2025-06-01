94ba978ed8c0a264dd262a5053e46852420268b5
Ekpunobi Gabriel <eodirah@gmail.com>
2025-06-01 15:47:18 +0100

    Corrected directory structure for ALX

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+# Assign values to the variables
+number1 = 3
+number2 = 5
+
+# Perform basic arithmethic operations
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+#print results in the specified format
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")

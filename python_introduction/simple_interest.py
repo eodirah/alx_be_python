ca2f6252cc1292e2ac0210063d53fbb8185da6cd
Ekpunobi Gabriel <eodirah@gmail.com>
2025-06-02 02:47:41 +0100

    Add simple_interest.py for ALX task

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,14 @@
+# simple interest formula: I = P * R * T
+
+#value declaration
+
+principal = P  = 1000
+rate = R = 0.05
+time = R =  3
+
+#asign value to the result simple interest as interest
+
+interest = principal * rate * time
+
+#print the requested format
+print(f"The simple interest is: {interest}")
